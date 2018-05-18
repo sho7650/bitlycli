1643e6af37fad2e67d890614d45ba172cacf7cf0
Takashi Abe <tabe@salesforce.com>
2018-05-18 17:53:06 +0900

    refactored a little

--- main.js
+++ main.js
@@ -2,16 +2,17 @@
 
 'use strict';
 
-const home_dir = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
 const request = require('request');
+const home_dir = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
 const bitly = require(home_dir + '/.bitly.json');
+const api_url = 'https://api-ssl.bitly.com/v3/shorten';
 const long_url = process.argv[process.argv.length - 1];
 const pattern = /^https?:\/\//;
 
 if (pattern.test(long_url)) {
   request.get(
     {
-      'url': 'https://api-ssl.bitly.com/v3/shorten',
+      'url': api_url,
       'qs': {
         'access_token': bitly.access_token,
         'longUrl': long_url
